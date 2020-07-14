86592f1dc13916db7128814b61c4bd29b5f2f256
Unknown <vadimkor@yandex.ru>
2020-07-14 11:00:30 +0300

    better compatibility of d1-icons.css

--- css-icons.js
+++ css-icons.js
@@ -13,8 +13,19 @@ stream.once('open', function(fd) {
   Object.keys(icons).forEach(name => {
     const size = icons[name][0];
     const path = icons[name][1];
-    /* svg+xml;utf-8, */
-    stream.write(".icon-" + name + ":before{background-image:url(\"data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 " + size + " " + size + "'><path d='" + path + "'></path></svg>\");}\n");
+    /*
+      https://codepen.io/tigt/post/optimizing-svgs-in-data-uris
+      - use single quotes inside SVG (URL-safe)
+      - wrap URI with double quotes: <img src="">, url("").
+      - do not use ";utf-8" (no need for ASCII)
+      - encode "<" as "%3C"
+      - encode ">" as "%3E"
+      - encode "#" as "%23"
+      - (use "rgb()" for colors) - Firefox
+    */
+    let svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 " + size + " " + size + "'><path fill='#00f' d='" + path + "'></path></svg>";
+    svg = svg.replace(/</g, '%3C').replace(/>/g, '%3E').replace(/#/g, '%23');
+    stream.write(".icon-" + name + ":before{background-image:url(\"data:image/svg+xml," + svg + "\");}\n");
   });
   stream.end();
 });
--- dist/d1-basic.js
+++ dist/d1-basic.js
@@ -1,4 +1,4 @@
-/*! d1-web v1.4.1 */
+/*! d1-web v1.4.2 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
